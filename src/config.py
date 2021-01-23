c2e32bcddfda1b70198af093a745799e6bc26e23
Jamsed Cabili <jamsedcabili@gmail.com>
2021-01-23 23:36:28 +0800

    Clean Data

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 # DATA SOURCE
-MRN_WOQ_CLEAN_PATH = "../data/source/mrnWOQClnd.txt"
-MRN_ALL_CLEAN_PATH = "../data/source/mrnAllClnd.txt"
-ENG_WOQ_CLEAN_PATH = "../data/source/engWOQClnd.txt"
+MRN_WOQ_CLEAN_PATH = "../clean_data/source/mrnWOQClnd.txt"
+MRN_ALL_CLEAN_PATH = "../clean_data/source/mrnAllClnd.txt"
+ENG_WOQ_CLEAN_PATH = "../clean_data/source/engWOQClnd.txt"
 
 # TOKENIZER 
 BPE_PATH = "../data/tokenizer/bpe/"
@@ -12,7 +12,7 @@ T5_LOGGING_PATH = "../logs/t5/"
 MRANAX_LOGGING_PATH = "../logs/mranax/"
 
 # MODEL PATH
-T5_MODEL_PATH = "../data/model/t5/"
-MRANAX_MODEL_PATH = "../data/model/mranax/"
+T5_MODEL_PATH = "../clean_data/model/t5/"
+MRANAX_MODEL_PATH = "../clean_data/model/mranax/"
 
 VOCAB_SIZE = 24500
