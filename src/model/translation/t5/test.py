e67c4a448527b024af402f9e840d3d4d025b9ec9
jamsed <jamsedcabili@gmail.com>
2021-01-25 19:12:47 +0800

    added inital model

--- src/model/translation/t5/test.py
+++ src/model/translation/t5/test.py
@@ -3,3 +3,14 @@ from transformers import (
     T5TokenizerFast
 )
 
+import config
+
+model = T5ForConditionalGeneration.from_pretrained(config.T5_MODEL_PATH_FINAL)
+tokenizer = T5TokenizerFast.from_pretrained("t5-small")
+
+
+mrn = "She feared the thunder."
+src = "translate English to Maranao: " + mrn
+tok = tokenizer(src, return_tensors="pt").input_ids
+res = model.generate(tok)
+print(tokenizer.decode(res[0]))
